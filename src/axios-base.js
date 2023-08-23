652bef1569c7ba6331f4276ff845a59978d11e58
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-08-23 11:06:50 +0800

    a

--- src/axios-base.js
+++ src/axios-base.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
-  // baseURL: "https://admin.germondental.com/api/",
+  // baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://admin.node.mn/api/",
 });
 
 instance.defaults.withCredentials = true;

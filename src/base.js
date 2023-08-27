0fdd3b2822e894a2f40b6343a544d8ad7dfaedcc
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-08-27 20:23:01 +0800

    a

--- src/base.js
+++ src/base.js
@@ -1,12 +1,12 @@
 const base = {
-  siteUrl: "http://localhost:3000/",
-  baseUrl: "http://localhost:3001/",
-  cdnUrl: "http://localhost:8000/uploads/",
-  apiUrl: "http://localhost:8000/v1/api/",
-  // siteUrl: "https://node.mn/",
-  // baseUrl: "https://admin.node.mn/",
-  // cdnUrl: "https://node.mn/uploads/",
-  // apiUrl: "https://admin.node.mn/api/",
+  // siteUrl: "http://localhost:3000/",
+  // baseUrl: "http://localhost:3001/",
+  // cdnUrl: "http://localhost:8000/uploads/",
+  // apiUrl: "http://localhost:8000/v1/api/",
+  siteUrl: "https://node.mn/",
+  baseUrl: "https://admin.node.mn/",
+  cdnUrl: "https://node.mn/uploads/",
+  apiUrl: "https://admin.node.mn/api/",
 };
 
 export default base;

e3e96c753b48111662a74057ba4123f4e6bcbf1f
Tuvshinbat <TUVSHINBAT596@GMAIL.COM>
2024-12-18 19:37:33 +0800

    merge

--- src/Pages/Products/Edit/index.js
+++ src/Pages/Products/Edit/index.js
@@ -293,7 +293,7 @@ const Edit = (props) => {
                             rules={[requiredRule]}
                           >
                             <Editor
-                              apiKey="2nubq7tdhudthiy6wfb88xgs36os4z3f4tbtscdayg10vo1o"
+                              apiKey="p3ooelc5v0jqfzosggvh1ug9vc9yqyipicx8qd90s45tqsho"
                               init={{
                                 height: 300,
                                 menubar: false,

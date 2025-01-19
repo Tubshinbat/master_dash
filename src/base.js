14edcf9905c3ff2c5ec5a58fb777175ca68033ac
Tuvshinbat <TUVSHINBAT596@GMAIL.COM>
2025-01-20 04:45:25 +0800

    mebmer

--- src/base.js
+++ src/base.js
@@ -1,12 +1,12 @@
 const base = {
-  siteUrl: "http://localhost:3000/",
-  baseUrl: "http://localhost:3001/",
-  cdnUrl: "http://localhost:1003/uploads/",
-  apiUrl: "http://localhost:1003/v1/api/",
-  // siteUrl: "https://node.mn/",
-  // baseUrl: "https://admin.node.mn/",
-  // cdnUrl: "https://node.mn/uploads/",
-  // apiUrl: "https://admin.node.mn/api/",
+  // siteUrl: "http://localhost:3000/",
+  // baseUrl: "http://localhost:3001/",
+  // cdnUrl: "http://localhost:1003/uploads/",
+  // apiUrl: "http://localhost:1003/v1/api/",
+  siteUrl: "https://node.mn/",
+  baseUrl: "https://admin.node.mn/",
+  cdnUrl: "https://node.mn/uploads/",
+  apiUrl: "https://admin.node.mn/api/",
 };
 
 export default base;
